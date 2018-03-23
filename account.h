4d0048fd356ad87a74b20d016302e8d2bcc52c8e
Madeline Farina <m.farina@ufl.edu>
2018-03-23 16:52:50 -0400

    Add files via upload

---
+++ account.h
@@ -0,0 +1,37 @@
+//
+//  account.h
+//  cop3503_termproject
+//
+//  Created by Madeline Farina on 3/23/18.
+//  Copyright © 2018 Madeline Farina. All rights reserved.
+//
+
+#ifndef account_h
+#define account_h
+
+#include <string>
+#include <cstdlib>
+#include <iostream>
+//#include "Expenses.h"
+//#include "user.h"
+
+class Account {
+    
+private:
+    double startBalance;
+    double incomingSalary;
+    //Expenses expenses; //can't uncomment until Expenses class is created
+    double savingAccountNumber;
+    double checkingAccountNumber;
+
+public:
+    void setStartBalance(double x);
+    void setIncomingSalary(double x);
+    double getIncomingSalary();
+    double getAccountNumber();
+    double deposite(double x);
+    //void enterExpenses(Expenses x);
+    //void transfer(User x);
+};
+
+#endif /* account_h */
