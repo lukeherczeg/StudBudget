011fb827cbde89c43a76d5b42f491d38f89b4d1e
cruales <christie.ruales@gmail.com>
2018-04-06 16:48:13 -0400

    added .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,6 @@ int main() {
   std::cout<<"Test"<<std::endl;
   std::cout << "Another test" << std::endl; // Luke 3/26
   std::cout << "And yet another test" << std::endl; //Madeline 4/6 @ 3:38pm
-  std::cout << "Testing git pushing" << std::endl; //Christie 4/6 @ 4:41pm
+  std::cout << "Testing git pushing(:" << std::endl; //Christie 4/6 @ 4:41pm
   return 0;
 }
