4d805ed4cc11e37b9b44e4b5cad77d6c092c45cb
Caitlin Becker <beckerc@ufl.edu>
2018-04-13 16:25:02 -0400

    Add files via upload

---
+++ src/expenses.cpp
@@ -0,0 +1,45 @@
+#include "expenses.h"
+
+private double foodCost
+private double rentCost
+private double entertainmentCost
+private double tutionCost
+private double savingsCost
+private double miscCost
+
+public void setFoodCost(double food) {
+  foodCost = food;
+}
+public double getFoodCost() {
+  return foodCost;
+}
+public void setRent(double rent) {
+  rentCost = rent;
+}
+public double getRent() {
+  return foodCost;
+}
+public void setEntertainmentCost(double entertainment) {
+  entertainmentCost = entertainment;
+}
+public double getEntertainmentCost() {
+  return entertainmentCost;
+}
+public void setTuitionCost(double tuition) {
+  tuitionCost = tuition;
+}
+public double getTutionCost() {
+  return tuitionCost;
+}
+public void setSavingsCost(double savings) {
+  savingsCost = savings;
+}
+public double getSavingsCost() {
+  return savingsCost;
+}
+public void setMiscCost(double misc) {
+  miscCost = misc;
+}
+public double getMiscCost() {
+  return miscCost;
+}
