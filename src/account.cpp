732a762c53745f78a37bdfd62e92f2848aece6ac
cruales <christie.ruales@gmail.com>
2018-04-15 17:33:13 -0400

    started account.cpp

--- src/account.cpp
+++ src/account.cpp
@@ -24,7 +24,34 @@ public:
     void transfer(User x);
 };*/
 
+void Account::setStartBalance(double startBalance){
+  this->startBalance = startBalance;
+}
 
+double Account::getStartBalance(){
+  return this->startBalance;
+}
 
+void Account::setIncomingSalary(double incomingSalary){
+  this->incomingSalary = incomingSalary;
+}
 
+double Account::getIncomingSalary(){
+  return this->incomingSalary;
+}
 
+void Account::setSavingAccountNumber(double savingAccountNumber){
+  this->savingAccountNumber = savingAccountNumber;
+}
+
+double Account::getSavingAccountNumber(){
+  return this->savingAccountNumber;
+}
+
+void Account::setCheckingAccountNumber(double checkingAccountNumber){
+  this->checkingAccountNumber = checkingAccountNumber;
+}
+
+double Account::getCheckingAccountNumber(){
+  return this->checkingAccountNumber;
+}
